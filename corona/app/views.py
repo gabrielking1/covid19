bcec711a25a8c836897c2e53a3c333e3144c8ed9
gabrielking1 <80740923+gabrielking1@users.noreply.github.com>
2023-02-18 14:37:50 +0100

    Update views.py

--- corona/app/views.py
+++ corona/app/views.py
@@ -42,29 +42,29 @@ def home(request):
         }
     )
 
-def contact(request):
-    """Renders the contact page."""
+# def contact(request):
+#     """Renders the contact page."""
 
-    assert isinstance(request, HttpRequest)
-    return render(
-        request,
-        'app/mapa.html',
-        {
-            'title':'Contact',
-            'message':'Your contact page.',
-            'year':datetime.now().year,
-        }
-    )
+#     assert isinstance(request, HttpRequest)
+#     return render(
+#         request,
+#         'app/mapa.html',
+#         {
+#             'title':'Contact',
+#             'message':'Your contact page.',
+#             'year':datetime.now().year,
+#         }
+#     )
 
-def about(request):
-    """Renders the about page."""
-    assert isinstance(request, HttpRequest)
-    return render(
-        request,
-        'app/about.html',
-        {
-            'title':'About',
-            'message':'Your application description page.',
-            'year':datetime.now().year,
-        }
-    )
+# def about(request):
+#     """Renders the about page."""
+#     assert isinstance(request, HttpRequest)
+#     return render(
+#         request,
+#         'app/about.html',
+#         {
+#             'title':'About',
+#             'message':'Your application description page.',
+#             'year':datetime.now().year,
+#         }
+#     )
